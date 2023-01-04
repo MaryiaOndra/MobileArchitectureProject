19c4e1f7ef50fc57267c75ae62c1b990de737a6b
Maryia Ondra <maryiaondra@gmail.com>
2023-01-04 13:23:14 +0400

    add GameFactory and AssetManagement

---
+++ Assets/CodeBase/Infrastructure/GameFactory.cs
@@ -0,0 +1,20 @@
+﻿using CodeBase.Infrastructure.AssetManagement;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    public class GameFactory : IGameFactory
+    {
+        private readonly IAssetProvider assets;
+
+        public GameFactory(IAssetProvider assets)
+        {
+            this.assets = assets;
+        }
+        public GameObject CreateHero(GameObject at) =>
+            assets.Instantiate(AssetPath.HeroPrefabPath, at.transform.position);
+
+        public void CreateHud() =>
+            assets.Instantiate(AssetPath.HudPath);
+    }
+}
--- Assets/CodeBase/Infrastructure/LoadLevelState.cs
+++ Assets/CodeBase/Infrastructure/LoadLevelState.cs
@@ -7,11 +7,10 @@ namespace CodeBase.Infrastructure
     public class LoadLevelState : IPayloadedState<string>
     {
         private const string InitialPointTag = "InitialTag";
-        private const string HeroPrefabPath = "Hero/Prefabs/Hero";
-        private const string HudPath = "Hud";
         private readonly GameStateMachine stateMachine;
         private readonly SceneLoader sceneLoader;
         private readonly LoadingCurtain loadingCurtain;
+        private IGameFactory gameFactory;
 
         public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, LoadingCurtain loadingCurtain)
         {
@@ -28,28 +27,17 @@ namespace CodeBase.Infrastructure
 
         private void OnLoaded()
         {
-            var initialPoint = GameObject.FindGameObjectWithTag(InitialPointTag);
-            GameObject hero = Instantiate(HeroPrefabPath, initialPoint.transform.position);
-            Instantiate(HudPath);
+            GameObject hero = gameFactory.CreateHero(at: GameObject.FindGameObjectWithTag(InitialPointTag));
+            gameFactory.CreateHud();
             CameraFollow(hero);
             stateMachine.Enter<GameLoopState>();
         }
+
         private void CameraFollow(GameObject hero)
         {
             Camera.main
                 .GetComponent<CameraFollow>()
                 .Follow(hero);
-        }
-
-        private static GameObject Instantiate(string path)
-        {
-            var heroPrefab = Resources.Load<GameObject>(path);
-            return Object.Instantiate(heroPrefab);
-        }
-        private static GameObject Instantiate(string path, Vector3 at)
-        {
-            var heroPrefab = Resources.Load<GameObject>(path);
-            return Object.Instantiate(heroPrefab, at, Quaternion.identity);
         }
 
         public void Exit() =>
